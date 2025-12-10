4229d00e580450f9ba181e53dd55b8063af9fa72
AbdoNady14 <abdonady140@gmail.com>
2025-12-10 13:21:31 +0200

    .

---
+++ Patron.java
@@ -0,0 +1,12 @@
+public class Patron extends User {
+    public Patron(String name, String password) {
+        super(name, password);
+    }
+    public BookStatus requestReservation(Book book) {
+        if (book.getStatus() == BookStatus.AVAILABLE) {
+            book.setStatus(BookStatus.RESERVED);
+            return BookStatus.RESERVED;
+        }
+        return BookStatus.RESERVED;
+    }
+}
